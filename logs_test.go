1ba035e9d7bb676cb0b3850c483bf41da44a8f02
vitams <vmpartner@gmail.com>
2020-08-10 19:34:38 +0500

    Tests

--- logs_test.go
+++ logs_test.go
@@ -1,11 +1,39 @@
 package logs_test
 
 import (
+	"encoding/json"
 	"github.com/moskvorechie/logs"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestInitLogsToFile(t *testing.T) {
-	l, _ := logs.New("main.log")
+func TestNew(t *testing.T) {
+	err := os.Remove("main.log")
+	if err != nil {
+		t.Fatal(err)
+	}
+	l, err := logs.New("main.log")
+	if err != nil {
+		t.Fatal(err)
+	}
 	l.Info("123")
+	body, err := ioutil.ReadFile("main.log")
+	if err != nil {
+		t.Fatal(err)
+	}
+	type Message struct {
+		Level    string `json:"level"`
+		Time     int    `json:"time"`
+		Datetime string `json:"datetime"`
+		Message  string `json:"message"`
+	}
+	var m Message
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if m.Message != "123" {
+		t.Fatal("Not eq")
+	}
 }
